529c6c5dcc10dbee4aa28b6db4949efc57f0992a
Hejinzefinance <Hejinze_finance@pku.edu.cn>
2021-12-13 15:28:49 +0800

    加入cash_greek的分析

--- 02_src/classes/backtest/backtestFramework.py
+++ 02_src/classes/backtest/backtestFramework.py
@@ -118,8 +118,42 @@ class BacktestFramework:
         ax1.set_ylabel('金额/元')
         ax2.set_ylabel('股票头寸/股')
         plt.show()
+        # cash_delta可视化
+        fig2, ax1 = self.init_canvas([0.08, 0.05, 0.85, 0.86])
+        ax1.plot(self.trade_dates,self.backtest_df['cash_delta'],linewidth=0.5,
+                 color='orange',label='cash_delta')
+        ax2 = ax1.twinx()
+        ax2.plot(self.trade_dates,self.backtest_df['stock_price'],linewidth=1,
+                 color='red',label='stock_price（右轴）')
+        t_data = self.backtest_df['cash_delta']
+        min_value = t_data.min()
+        max_value = t_data.max()
+        ax1.vlines(self.trade_ticks,min_value,max_value,label='hedge_tradings',
+                   color='black',linewidths=0.1)
+        ax1.set_ylabel('cash_delta')
+        ax2.set_ylabel('stock_price')
+        ax1.set_title('cash_delta分析，，对冲策略:{0:s}\n{1:s}'.format(self.strategy_name,self.option_portfolio.option_info))
+        fig2.legend(loc='right')
+        plt.show()
+        # cash_gamma可视化
+        fig3, ax1 = self.init_canvas([0.08, 0.05, 0.85, 0.86])
+        ax1.plot(self.trade_dates,self.backtest_df['cash_gamma'],linewidth=0.5,
+                 color='green',label='cash_gamma')
+        ax2 = ax1.twinx()
+        ax2.plot(self.trade_dates,self.backtest_df['stock_price'],linewidth=1,
+                 color='red',label='stock_price（右轴）')
+        t_data = self.backtest_df['cash_gamma']
+        min_value = t_data.min()
+        max_value = t_data.max()
+        ax1.vlines(self.trade_ticks,min_value,max_value,label='hedge_tradings',
+                   color='black',linewidths=0.1)
+        ax1.set_ylabel('cash_gamma')
+        ax2.set_ylabel('stock_price')
+        ax1.set_title('cash_gamma分析，，对冲策略:{0:s}\n{1:s}'.format(self.strategy_name,self.option_portfolio.option_info))
+        fig3.legend(loc='right')
+        plt.show()
         # 收益拆分
-        fig2,ax1 = self.init_canvas([0.09,0.05,0.88,0.81])
+        fig4,ax1 = self.init_canvas([0.09,0.05,0.88,0.81])
         ax1.plot(self.trade_dates,self.backtest_df['nav'],linewidth=1,
                  color='red',label='整体盈利')
         ax1.plot(self.trade_dates,self.backtest_df['delta_value'],linewidth=0.5,
@@ -143,7 +177,7 @@ class BacktestFramework:
             self.analysis_dict['trading_cost'],self.backtest_df.option_value.iloc[0]))
         plt.show()
         # delta敞口收益分析
-        fig3,ax1 = self.init_canvas([0.09,0.05,0.88,0.81])
+        fig5,ax1 = self.init_canvas([0.09,0.05,0.88,0.81])
         ax1.bar(2,self.analysis_dict['delta_pnl_part1'],color='red')
         ax1.bar(4,self.analysis_dict['delta_pnl_part2'],color='orange')
         ax1.bar(6,self.analysis_dict['delta_pnl_part3'],color='green')
@@ -161,7 +195,7 @@ class BacktestFramework:
             self.analysis_dict['gamma_pnl'],self.analysis_dict['other_pnl'],self.analysis_dict['trading_cost']))
         plt.show()
         # delta敞口收益散点图
-        fig4,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
+        fig6,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
         t_data = self.backtest_df.loc[:,['trade_type','delta_pnl_contribution','gamma_pnl','other_pnl']].copy()
         t_data['delta_price'] = self.backtest_df.loc[:,'stock_price'].diff()
         t_data['total_cash_delta'] = self.backtest_df.loc[:,'total_cash_delta'].shift(1)
@@ -191,7 +225,7 @@ class BacktestFramework:
             self.strategy_name,self.option_portfolio.option_info))
         plt.show()
         # 1类收益占比分析
-        fig5,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
+        fig7,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
         ax1.plot(t_data.loc[t_data.trade_type==1].index,
                  t_data.loc[t_data.trade_type==1,'delta_pnl_contribution'],
                  color='red',label='cash_delta>0&△S>0类收益贡献比例')
@@ -201,7 +235,7 @@ class BacktestFramework:
             self.strategy_name,self.option_portfolio.option_info))
         plt.show()
         # 2类收益占比分析
-        fig6,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
+        fig8,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
         ax1.plot(t_data.loc[t_data.trade_type==2].index,
                  t_data.loc[t_data.trade_type==2,'delta_pnl_contribution'],
                  color='orange',label='cash_delta<0&△S>0类收益贡献比例')
@@ -211,7 +245,7 @@ class BacktestFramework:
             self.strategy_name,self.option_portfolio.option_info))
         plt.show()
         # 3类收益占比分析
-        fig7,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
+        fig9,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
         ax1.plot(t_data.loc[t_data.trade_type==3].index,
                  t_data.loc[t_data.trade_type==3,'delta_pnl_contribution'],
                  color='green',label='cash_delta<0&△S<0类收益贡献比例')
@@ -221,7 +255,7 @@ class BacktestFramework:
             self.strategy_name,self.option_portfolio.option_info))
         plt.show()
         # 4类收益占比分析
-        fig8,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
+        fig10,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
         ax1.plot(t_data.loc[t_data.trade_type==4].index,
                  t_data.loc[t_data.trade_type==4,'delta_pnl_contribution'],
                  color='blue',label='cash_delta>0&△S<0类收益贡献比例')
@@ -231,7 +265,7 @@ class BacktestFramework:
             self.strategy_name,self.option_portfolio.option_info))
         plt.show()
         # gamma收益分析
-        fig9,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
+        fig11,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
         ax1.plot(t_data.index,t_data.loc[:,'gamma_pnl'],
                  color='purple',label='gamma日收益')
         ax1.set_ylabel('收益/元')
@@ -240,7 +274,7 @@ class BacktestFramework:
             self.strategy_name,self.option_portfolio.option_info))
         plt.show()
         # 其他收益
-        fig10,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
+        fig12,ax1 = self.init_canvas([0.09,0.08,0.88,0.84])
         ax1.plot(t_data.index,t_data.loc[:,'other_pnl'],
                  color='brown',label='其他日收益')
         ax1.set_ylabel('收益/元')
@@ -263,6 +297,8 @@ class BacktestFramework:
                 pdf.savefig(fig8)
                 pdf.savefig(fig9)
                 pdf.savefig(fig10)
+                pdf.savefig(fig11)
+                pdf.savefig(fig12)
         
     def init_analysis(self):
         self.analysis_dict = dict().fromkeys(self.analysis_index)
